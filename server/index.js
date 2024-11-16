ea598d12e8a89e671c1423a6cf1a2ab860ed06d9
K2s <elieserhs999omega@gmail.com>
2024-11-16 17:12:38 -0500

    v6.1

--- server/index.js
+++ server/index.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+// Elimine esto de las dependencias 
+// "morgan": "^1.10.0",
